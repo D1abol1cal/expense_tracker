3b518de98aa9a8cd20a0ec9c245714661029ad27
Nofel <syednofeltalha2@gmail.com>
2024-03-21 07:27:39 +0500

    Modal Overlay when add button is pressed

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,10 +1,9 @@
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
-import 'package:flutter/widgets.dart';
 
 class Expenses extends StatefulWidget {
-  const Expenses({Key? key}) : super(key: key);
+  const Expenses({super.key});
   @override
   _ExpensesState createState() => _ExpensesState();
 }
@@ -31,12 +30,19 @@ class _ExpensesState extends State<Expenses> {
     ),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const Text('Modal Bottom sheet'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text('Expense Tracker'), actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _openAddExpenseOverlay,
             icon: const Icon(Icons.add),
           ),
         ]),
